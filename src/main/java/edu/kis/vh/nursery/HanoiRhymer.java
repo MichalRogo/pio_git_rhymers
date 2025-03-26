3a6093ca8c0022f831eb03034eba3a1fb7c75cd1
Michał R. <252821@edu.p.lodz.pl>
2025-03-26 19:05:24 +0100

    Fixed issues caused by the rebase conflict

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,10 +1,6 @@
 package edu.kis.vh.nursery;
 
-<<<<<<< HEAD
 public class HanoiRhymer extends DefaultCountingOutRhymer{
-=======
-public class HanoiRhymer extends DefaultCountingOutRhymer {
->>>>>>> 6bd7702 (4.1 poprawa nazw klas)
 
     int totalRejected = 0;
 
